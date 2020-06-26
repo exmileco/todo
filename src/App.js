8e57cd98abee4dd85f5b0b17878917a8c87c8a15
Aidar Issayev <exmile.co@gmail.com>
2020-06-26 20:41:46 +0300

    Edited App.js

--- src/App.js
+++ src/App.js
@@ -3,23 +3,29 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Добро пожаловать в React
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return ( <
+    div className = "App" >
+    <
+    header className = "App-header" >
+    <
+    img src = {
+      logo
+    }
+    className = "App-logo"
+    alt = "logo" / >
+    <
+    p >
+    Добро пожаловать в React приложение!
+    <
+    /p> <
+    a className = "App-link"
+    href = "https://reactjs.org"
+    target = "_blank"
+    rel = "noopener noreferrer" >
+    Learn React <
+    /a> <
+    /header> <
+    /div>
   );
 }
 
